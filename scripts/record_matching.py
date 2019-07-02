a9d163122b4bacd41da39673e68a9dbf807bc0aa
LKillaars <27420806+Killaars@users.noreply.github.com>
2019-07-02 19:28:09 +0200

    recordmatching working

--- scripts/record_matching.py
+++ scripts/record_matching.py
@@ -9,14 +9,17 @@ import datetime
 import recordlinkage
 from recordlinkage.index import Full
 
+import dask.dataframe as dd
+from dask.multiprocessing import get
+
 nltk.download('punkt')
 
 from project_functions import preprocessing, check_sleutelwoorden,expand_parents_df,correct,sleutelwoorden_routine
 
 #%%
 
-#path = Path('/Users/Lars/Documents/CBS/CBS2_mediakoppeling/data/')
-path = Path('/Users/rwsla/Lars/CBS_2_mediakoppeling/data/solr/')
+path = Path('/Users/Lars/Documents/CBS/CBS2_mediakoppeling/data/')
+#path = Path('/Users/rwsla/Lars/CBS_2_mediakoppeling/data/solr/')
 # Variables
 upwindow = 7
 lowwindow = 2
@@ -200,6 +203,7 @@ subset.loc[:,'cbs_link'] = subset.apply(find_link,axis=1)
 indexer = recordlinkage.Index()
 indexer.add(Full())
 candidate_links = indexer.index(parents_rel_columns, subset)
+print('Done with indexing')
 
 # comparison step
 compare_cl = recordlinkage.Compare()
@@ -209,6 +213,7 @@ compare_cl.string('link', 'cbs_link', method='jarowinkler', threshold=0.93, labe
 
 features = compare_cl.compute(candidate_links, parents_rel_columns, subset)
 features.reset_index(inplace=True)
+print('Done with comparing')
 
 # add extra data of parents and children to feature table and rename conflicting columns
 features.loc[:,'child_id'] = features.apply(find_id,args=(subset,'level_1'),axis=1)
@@ -222,16 +227,106 @@ features.rename(columns={'title_x': 'title_parent',
                          'title_y': 'title_child',
                          'content_y': 'content_child',
                          'publish_date_date_y': 'publish_date_date_child'}, inplace=True)
+#%%
+# Setting up multiprocessing
+ddata = dd.from_pandas(features, npartitions=16)
     
 # determine other features
 #features = features.tail(1)
-features['feature_whole_title'] = features.apply(find_title,axis=1)
+a = datetime.datetime.now()
+print('Done with preprocessing')
+#features['feature_whole_title'] = features.apply(find_title,axis=1)
+features['feature_whole_title'] = ddata.map_partitions(lambda df: df.apply(lambda row: find_title, axis=1)).compute(scheduler='processes')
+print('Done with whole title')
+#features[['sleutelwoorden_jaccard','sleutelwoorden_lenmatches','sleutelwoorden_matches']] = features.apply(find_sleutelwoorden_UF,axis=1)
+#print('Done with sleutelwoorden')
+#features[['BT_TT_jaccard','BT_TT_lenmatches','BT_TT_matches']] = features.apply(find_BT_TT,axis=1)
+#print('Done with BT_TT')
+#features[['title_no_stop_jaccard','title_no_stop_lenmatches','title_no_stop_matches']] = features.apply(find_title_no_stop,axis=1)
+#print('Done with title no stop')
+#features[['1st_paragraph_no_stop_jaccard','1st_paragraph_no_stop_lenmatches','1st_paragraph_no_stop_matches']] = features.apply(find_1st_paragraph_no_stop,axis=1)
+#print('Done with paragraph no stop')
+#features['match'] = features.apply(determine_matches,axis=1)
+#print('Done with determining matches')
+#features['date_diff_days'] = abs(features['publish_date_date_parent']-features['publish_date_date_child']).dt.days.astype(float)
+
+#offset = 0
+#scale = 7
+#features['date_diff_score'] = features.apply(date_comparison,args=(offset,scale),axis=1)
+#print('Done with diff_dates')
+
+b = datetime.datetime.now()
+c=b-a
+print(c)
+
+#%% Multicore
+npartitions = 16
+# subset of children
+subset = children_rel_columns.head(1000)
+
+# preprocessing
+subset.loc[:,'publish_date_date'] = pd.to_datetime(subset.loc[:,'publish_date_date'])
+parents_rel_columns.loc[:,'publish_date_date'] = pd.to_datetime(parents_rel_columns.loc[:,'publish_date_date'])
+
+# expand children columns
+ddata = dd.from_pandas(subset, npartitions=npartitions)
+subset.loc[:,'cbs_link'] = ddata.map_partitions(lambda df: df.apply(lambda row: find_link(row), axis=1)).compute(scheduler='processes')
+
+# indexation step
+indexer = recordlinkage.Index()
+indexer.add(Full())
+candidate_links = indexer.index(parents_rel_columns, subset)
+print('Done with indexing')
+
+# comparison step
+compare_cl = recordlinkage.Compare()
+compare_cl.string('link', 'cbs_link', method='jarowinkler', threshold=0.93, label='feature_link_score')
+
+features = compare_cl.compute(candidate_links, parents_rel_columns, subset)
+features.reset_index(inplace=True)
+print('Done with comparing')
+
+# add extra data of parents and children to feature table and rename conflicting columns
+features.loc[:,'child_id'] = features.apply(find_id,args=(subset,'level_1'),axis=1)
+features.loc[:,'parent_id'] = features.apply(find_id,args=(parents_rel_columns,'level_0'),axis=1)
+features = features.merge(parents_rel_columns, left_on = 'parent_id', right_on = 'id', how = 'left')
+features = features.merge(subset, left_on = 'child_id', right_on = 'id', how = 'left')
+features.drop(columns = ['level_0','level_1','id_x','id_y'],inplace=True)
+features.rename(columns={'title_x': 'title_parent',
+                         'content_x': 'content_parent',
+                         'publish_date_date_x': 'publish_date_date_parent',
+                         'title_y': 'title_child',
+                         'content_y': 'content_child',
+                         'publish_date_date_y': 'publish_date_date_child'}, inplace=True)
+
+ddata = dd.from_pandas(features, npartitions=npartitions)
+# determine other features
+#features = features.tail(1)
+a = datetime.datetime.now()
+print('Done with preprocessing')
+#features['feature_whole_title'] = features.apply(find_title,axis=1)
+features['feature_whole_title'] = ddata.map_partitions(lambda df: df.apply(lambda row: find_title(row), axis=1)).compute(scheduler='processes')
+print('Done with whole title')
 features[['sleutelwoorden_jaccard','sleutelwoorden_lenmatches','sleutelwoorden_matches']] = features.apply(find_sleutelwoorden_UF,axis=1)
+#features[['sleutelwoorden_jaccard','sleutelwoorden_lenmatches','sleutelwoorden_matches']] = ddata.map_partitions(lambda df: df.apply(lambda row: find_sleutelwoorden_UF(row), axis=1)).compute(scheduler='processes')
+print('Done with sleutelwoorden')
 features[['BT_TT_jaccard','BT_TT_lenmatches','BT_TT_matches']] = features.apply(find_BT_TT,axis=1)
+#features[['BT_TT_jaccard','BT_TT_lenmatches','BT_TT_matches']] = ddata.map_partitions(lambda df: df.apply(lambda row: find_BT_TT(row), axis=1)).compute(scheduler='processes')
+print('Done with BT_TT')
 features[['title_no_stop_jaccard','title_no_stop_lenmatches','title_no_stop_matches']] = features.apply(find_title_no_stop,axis=1)
+print('Done with title no stop')
 features[['1st_paragraph_no_stop_jaccard','1st_paragraph_no_stop_lenmatches','1st_paragraph_no_stop_matches']] = features.apply(find_1st_paragraph_no_stop,axis=1)
+print('Done with paragraph no stop')
 features['match'] = features.apply(determine_matches,axis=1)
+print('Done with determining matches')
 features['date_diff_days'] = abs(features['publish_date_date_parent']-features['publish_date_date_child']).dt.days.astype(float)
+
 offset = 0
 scale = 7
 features['date_diff_score'] = features.apply(date_comparison,args=(offset,scale),axis=1)
+print('Done with diff_dates')
+
+b = datetime.datetime.now()
+c=b-a
+print(c)
+
